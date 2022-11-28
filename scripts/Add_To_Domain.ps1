92d27ddec9a4f3ef5eef70e6c9e9b02024d93393
Pierre Smit <pierre.smit@outlook.com>
2022-11-28 15:04:43 +0200

    2

--- scripts/Add_To_Domain.ps1
+++ scripts/Add_To_Domain.ps1
@@ -1,16 +1,14 @@
 
-If (!(Get-CimInstance -Class Win32_ComputerSystem).PartOfDomain) {
 try {
-    Write-Host -ForegroundColor Red "This machine is not part of a domain. Adding now."
-    $user = Read-Host "Userid"
-    $pass = Read-Host "Password" -AsSecureString
-	$labcred = New-Object System.Management.Automation.PSCredential ($user,$pass)
+    Write-Host -ForegroundColor Red 'This machine is not part of a domain. Adding now.'
+    $user = Read-Host 'Userid'
+    $pass = Read-Host 'Password' -AsSecureString
+    $labcred = New-Object System.Management.Automation.PSCredential ($user, $pass)
     
-    Rename-Computer -ComputerName $env:COMPUTERNAME -NewName "Dev-$(get-random -Maximum 5000)"
+    Rename-Computer -ComputerName $env:COMPUTERNAME -NewName "Dev-$(Get-Random -Maximum 5000)"
     Start-Sleep 5
-    Add-Computer -DomainName 'internal.lab' -Credential $labcred -Options JoinWithNewName,AccountCreate -Force -ErrorAction Stop
-        if (Test-PendingReboot) {
-            Invoke-Reboot
-        }
-   } catch {Write-Warning "unable to add to the domain."}
-}
+    Add-Computer -DomainName 'internal.lab' -Credential $labcred -Options JoinWithNewName, AccountCreate -Force -ErrorAction Stop
+    if (Test-PendingReboot) {
+        Invoke-Reboot
+    }
+} catch {Write-Warning 'unable to add to the domain.'}
