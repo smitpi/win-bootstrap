a10a945b98d251013d5e466b865990c2e4c90495
Pierre Smit <pierre.smit@outlook.com>
2022-12-06 21:38:53 +0200

    r

---
+++ dev_lab_setup.sync-conflict-20221201-155053-E24EPFO.ps1
@@ -0,0 +1,52 @@
+# Description: Boxstarter Script
+# Author: Pierre
+# Common dev settings for desktop app development
+
+# Boxstarter options
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+
+Disable-UAC
+$ConfirmPreference = "None" #ensure installing powershell modules don't prompt on needed dependencies
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	Invoke-Expression ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+# Workaround choco / boxstarter path too long error
+# https://github.com/chocolatey/boxstarter/issues/241
+$chocoCachePath = "$env:TEMP"
+
+if([string]::IsNullOrEmpty($chocoCachePath)) {
+    $chocoCachePath = "$env:USERPROFILE\AppData\Local\Temp\chocolatey"
+}
+Write-Host "Using chocoCachePath: $chocoCachePath"
+
+if (-not (Test-Path -LiteralPath $chocoCachePath)) {
+    Write-Host "Creating chocoCachePath dir."
+    New-Item -Path $chocoCachePath -ItemType Directory -Force
+}
+
+
+#--- Setting up Windows ---
+#executeScript "Add_To_Domain.ps1";
+executeScript "PStoolkit_Start.ps1";
+
+#--- reenabling critial items ---
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
