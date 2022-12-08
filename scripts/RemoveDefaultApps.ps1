b70acd48a27c9cc9aeca4c86d8d48286b60ed333
smitpi <pierre.smit@outlook.com>
2022-12-08 21:17:08 +0200

    Update RemoveDefaultApps.ps1

--- scripts/RemoveDefaultApps.ps1
+++ scripts/RemoveDefaultApps.ps1
@@ -11,8 +11,14 @@ Write-Host 'Uninstall some applications that come with Windows out of the box' -
 function removeApp {
 	Param ([string]$appName)
 	Write-Output "Trying to remove $appName"
-	Get-AppxPackage $appName -AllUsers -ErrorAction silentlycontinue | Remove-AppxPackage -ErrorAction silentlycontinue
-	Get-AppxProvisionedPackage -Online -ErrorAction silentlycontinue | Where-Object DisplayName -Like $appName | Remove-AppxProvisionedPackage -Online -ErrorAction silentlycontinue
+	try {
+		Get-AppxPackage $appName -AllUsers | Remove-AppxPackage
+	} catch {Write-Warning "Error Uninstalling $($appName): Message:$($Error[0])"}
+
+	try {
+		Get-AppxProvisionedPackage -Online | Where-Object DisplayName -Like $appName | Remove-AppxProvisionedPackage -Online
+	} catch {Write-Warning "Error Uninstalling $($appName): Message:$($Error[0])"}
+
 }
 
 $applicationList = @(
@@ -58,6 +64,126 @@ $applicationList = @(
 	'*.AdobePhotoshopExpress'
 );
 
-foreach ($app in $applicationList) {
+$applicationList2 = @(
+	# default Windows 10 apps
+	'Microsoft.549981C3F5F10' #Cortana
+	'Microsoft.3DBuilder'
+	'Microsoft.Appconnector'
+	'Microsoft.BingFinance'
+	'Microsoft.BingNews'
+	'Microsoft.BingSports'
+	'Microsoft.BingTranslator'
+	'Microsoft.BingWeather'
+	#"Microsoft.FreshPaint"
+	'Microsoft.GamingServices'
+	'Microsoft.MicrosoftOfficeHub'
+	'Microsoft.MicrosoftPowerBIForWindows'
+	'Microsoft.MicrosoftSolitaireCollection'
+	#"Microsoft.MicrosoftStickyNotes"
+	'Microsoft.MinecraftUWP'
+	'Microsoft.NetworkSpeedTest'
+	'Microsoft.Office.OneNote'
+	'Microsoft.People'
+	'Microsoft.Print3D'
+	'Microsoft.SkypeApp'
+	'Microsoft.Wallet'
+	#"Microsoft.Windows.Photos"
+	'Microsoft.WindowsAlarms'
+	#"Microsoft.WindowsCalculator"
+	'Microsoft.WindowsCamera'
+	'microsoft.windowscommunicationsapps'
+	'Microsoft.WindowsMaps'
+	'Microsoft.WindowsPhone'
+	'Microsoft.WindowsSoundRecorder'
+	#"Microsoft.WindowsStore"   # can't be re-installed
+	'Microsoft.Xbox.TCUI'
+	'Microsoft.XboxApp'
+	'Microsoft.XboxGameOverlay'
+	'Microsoft.XboxSpeechToTextOverlay'
+	'Microsoft.YourPhone'
+	'Microsoft.ZuneMusic'
+	'Microsoft.ZuneVideo'
+
+	# Threshold 2 apps
+	'Microsoft.CommsPhone'
+	'Microsoft.ConnectivityStore'
+	'Microsoft.GetHelp'
+	'Microsoft.Getstarted'
+	'Microsoft.Messaging'
+	'Microsoft.Office.Sway'
+	'Microsoft.OneConnect'
+	'Microsoft.WindowsFeedbackHub'
+
+	# Creators Update apps
+	'Microsoft.Microsoft3DViewer'
+	#"Microsoft.MSPaint"
+
+	#Redstone apps
+	'Microsoft.BingFoodAndDrink'
+	'Microsoft.BingHealthAndFitness'
+	'Microsoft.BingTravel'
+	'Microsoft.WindowsReadingList'
+
+	# Redstone 5 apps
+	'Microsoft.MixedReality.Portal'
+	'Microsoft.ScreenSketch'
+	'Microsoft.XboxGamingOverlay'
+
+	# non-Microsoft
+	'2FE3CB00.PicsArt-PhotoStudio'
+	'46928bounde.EclipseManager'
+	'4DF9E0F8.Netflix'
+	'613EBCEA.PolarrPhotoEditorAcademicEdition'
+	'6Wunderkinder.Wunderlist'
+	'7EE7776C.LinkedInforWindows'
+	'89006A2E.AutodeskSketchBook'
+	'9E2F88E3.Twitter'
+	'A278AB0D.DisneyMagicKingdoms'
+	'A278AB0D.MarchofEmpires'
+	'ActiproSoftwareLLC.562882FEEB491' # next one is for the Code Writer from Actipro Software LLC
+	'ClearChannelRadioDigital.iHeartRadio'
+	'D52A8D61.FarmVille2CountryEscape'
+	'D5EA27B7.Duolingo-LearnLanguagesforFree'
+	'DB6EA5DB.CyberLinkMediaSuiteEssentials'
+	'DolbyLaboratories.DolbyAccess'
+	'DolbyLaboratories.DolbyAccess'
+	'Drawboard.DrawboardPDF'
+	'Facebook.Facebook'
+	'Fitbit.FitbitCoach'
+	'Flipboard.Flipboard'
+	'GAMELOFTSA.Asphalt8Airborne'
+	'KeeperSecurityInc.Keeper'
+	'NORDCURRENT.COOKINGFEVER'
+	'PandoraMediaInc.29680B314EFC2'
+	'Playtika.CaesarsSlotsFreeCasino'
+	'ShazamEntertainmentLtd.Shazam'
+	'SlingTVLLC.SlingTV'
+	'SpotifyAB.SpotifyMusic'
+	#"TheNewYorkTimes.NYTCrossword"
+	'ThumbmunkeysLtd.PhototasticCollage'
+	'TuneIn.TuneInRadio'
+	'WinZipComputing.WinZipUniversal'
+	'XINGAG.XING'
+	'flaregamesGmbH.RoyalRevolt2'
+	'king.com.*'
+	'king.com.BubbleWitch3Saga'
+	'king.com.CandyCrushSaga'
+	'king.com.CandyCrushSodaSaga'
+
+	# apps which cannot be removed using Remove-AppxPackage
+	#"Microsoft.BioEnrollment"
+	#"Microsoft.MicrosoftEdge"
+	#"Microsoft.Windows.Cortana"
+	#"Microsoft.WindowsFeedback"
+	#"Microsoft.XboxGameCallableUI"
+	#"Microsoft.XboxIdentityProvider"
+	#"Windows.ContactSupport"
+
+	# apps which other apps depend on
+	'Microsoft.Advertising.Xaml'
+)
+      
+
+foreach ($app in $applicationList2) {
 	removeApp $app
 }
