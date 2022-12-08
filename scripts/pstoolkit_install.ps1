556b3d95b57642ae4df69d8d7c7e3a9ef13f2912
smitpi <pierre.smit@outlook.com>
2022-12-08 20:33:24 +0200

    Update pstoolkit_install.ps1

--- scripts/pstoolkit_install.ps1
+++ scripts/pstoolkit_install.ps1
@@ -1,29 +1,41 @@
-	#region Needed Modules
-	Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host '[Installing]: ' -NoNewline -ForegroundColor Yellow; Write-Host "Needed PowerShell Modules`n" -ForegroundColor Cyan
-	'ImportExcel', 'PSWriteHTML', 'PSWriteColor', 'PSScriptTools', 'PoshRegistry', 'Microsoft.PowerShell.Archive', 'PWSHModule', 'PSPackageMan' | ForEach-Object {		
-		$module = $_
-		if (-not(Get-Module $module) -and -not(Get-Module $module -ListAvailable)) {
-			try {
-				Write-Host "`t[Installing]: " -NoNewline -ForegroundColor Yellow; Write-Host "$($module):" -ForegroundColor Cyan -NoNewline
-				Install-Module -Name $module -Scope AllUsers -AllowClobber -ErrorAction stop
-				Write-Host ' Complete' -ForegroundColor Green
-			} catch {Write-Warning "Error installing module $($module): Message:$($Error[0])"}
-		} else {
-			Write-Host "`t[Installing]: " -NoNewline -ForegroundColor Yellow; Write-Host "$($module):" -ForegroundColor Cyan -NoNewline; Write-Host ' Already Installed' -ForegroundColor Red
-		}
+#region Needed Modules
+Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host '[Installing]: ' -NoNewline -ForegroundColor Yellow; Write-Host "Needed PowerShell Modules`n" -ForegroundColor Cyan
+'ImportExcel', 'PSWriteHTML', 'PSWriteColor', 'PSScriptTools', 'PoshRegistry', 'Microsoft.PowerShell.Archive', 'PWSHModule', 'PSPackageMan' | ForEach-Object {		
+	$module = $_
+	if (-not(Get-Module $module) -and -not(Get-Module $module -ListAvailable)) {
+		try {
+			Write-Host "`t[Installing]: " -NoNewline -ForegroundColor Yellow; Write-Host "$($module):" -ForegroundColor Cyan -NoNewline
+			Install-Module -Name $module -Scope AllUsers -AllowClobber -ErrorAction stop
+			Write-Host ' Complete' -ForegroundColor Green
+		} catch {Write-Warning "Error installing module $($module): Message:$($Error[0])"}
+	} else {
+		Write-Host "`t[Installing]: " -NoNewline -ForegroundColor Yellow; Write-Host "$($module):" -ForegroundColor Cyan -NoNewline; Write-Host ' Already Installed' -ForegroundColor Red
 	}
-	#endregion
+}
+#endregion
 
-	#region PStoolkit
-	Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host '[Installing]: ' -NoNewline -ForegroundColor Yellow; Write-Host "PSToolKit Module`n" -ForegroundColor Cyan
-	$web = New-Object System.Net.WebClient
-	$web.DownloadFile('https://raw.githubusercontent.com/smitpi/PSToolKit/master/PSToolKit/Public/Update-MyModulesFromGitHub.ps1', "$($env:TEMP)\Update-MyModulesFromGitHub.ps1")
-	$full = Get-Item "$($env:TEMP)\Update-MyModulesFromGitHub.ps1"
-	Import-Module $full.FullName -Force
-	Update-MyModulesFromGitHub -Modules PSToolkit -AllUsers
-	Remove-Item $full.FullName
-	Import-Module PSToolKit -Force
-	#endregion
+#region PStoolkit
+Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host '[Installing]: ' -NoNewline -ForegroundColor Yellow; Write-Host "PSToolKit Module`n" -ForegroundColor Cyan
+$ModulePathCheck = 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit' 		
+$PSTemp = "$env:tmp\PSModules"
+if (Test-Path $PSTemp) {Remove-Item $PSTemp -Force -Recurse}
+if (Test-Path $ModulePathCheck) {Remove-Item $ModulePathCheck -Force -Recurse}
+			
+$PSDownload = New-Item $PSTemp -ItemType Directory -Force
+$ModulePath = New-Item $ModulePathCheck -ItemType Directory -Force
+$ModuleTMPDest = Join-Path $PSDownload.FullName -ChildPath 'PSToolKit.zip'
+	
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile('https://github.com/smitpi/PSToolKit/archive/refs/heads/master.zip', $ModuleTMPDest)
+	
+Expand-Archive -Path $ModuleTMPDest -DestinationPath $PSDownload.FullName -Force
+$NewModule = Get-ChildItem -Directory "$($PSDownload.FullName)\PSToolKit-master\Output"
+Copy-Item -Path $NewModule.FullName -Destination $ModulePath.FullName -Recurse -Force
+Remove-Item $PSDownload -Force -Recurse
 
-	Set-PSToolKitSystemSetting -RunAll
-	New-PSProfile
+Import-Module PSToolKit -Force
+Show-PSToolKit -ShowMetaData
+#endregion
+
+Set-PSToolKitSystemSetting -RunAll
+New-PSProfile
