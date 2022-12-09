39298c78ef2dcf17d33a5aa09d779c5a939d9fb6
smitpi <pierre.smit@outlook.com>
2022-12-09 07:05:32 +0200

    Update dev_lab_setup.ps1

--- dev_lab_setup.ps1
+++ dev_lab_setup.ps1
@@ -9,47 +9,53 @@ $Boxstarter.AutoLogin = $true # Save my password securely and auto-login after a
 
 #. { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
 
-
-Disable-UAC
-$ConfirmPreference = 'None' #ensure installing PowerShell modules don't prompt on needed dependencies
+try {
+    Disable-UAC
+    $ConfirmPreference = 'None' #ensure installing PowerShell modules don't prompt on needed dependencies
+} catch {Write-Warning "Error: Message:$($Error[0])"}
 
 # Get the base URI path from the ScriptToCall value
-$bstrappackage = '-bootstrapPackage'
-$helperUri = $Boxstarter['ScriptToCall']
-$strpos = $helperUri.IndexOf($bstrappackage)
-$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
-$helperUri = $helperUri.TrimStart("'", ' ')
-$helperUri = $helperUri.TrimEnd("'", ' ')
-$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf('/'))
-$helperUri += '/scripts'
-Write-Host "helper script base URI is $helperUri"
-
+try {
+    $bstrappackage = '-bootstrapPackage'
+    $helperUri = $Boxstarter['ScriptToCall']
+    $strpos = $helperUri.IndexOf($bstrappackage)
+    $helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+    $helperUri = $helperUri.TrimStart("'", ' ')
+    $helperUri = $helperUri.TrimEnd("'", ' ')
+    $helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf('/'))
+    $helperUri += '/scripts'
+    Write-Host "helper script base URI is $helperUri"
+} catch {Write-Warning "Error: Message:$($Error[0])"}
 function executeScript {
     Param ([string]$script)
+    try {
+        $VerbosePreference = 'SilentlyContinue'
+        Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host "`t`t[Executing Script]: " -NoNewline -ForegroundColor Yellow; Write-Host "$($helperUri)/$($script)`n" -ForegroundColor Cyan
+        Invoke-Expression ((New-Object net.webclient).DownloadString("$helperUri/$script"))
+    } catch {Write-Warning "Error in $($script): Message:$($Error[0])"}
+}
+
+#region choco cache path
+try {
+    $chocoCachePath = "$env:TEMP"
+
+    if ([string]::IsNullOrEmpty($chocoCachePath)) {
+        $chocoCachePath = "$env:USERPROFILE\AppData\Local\Temp\chocolatey"
+    }
+    Write-Host "`n`nUsing chocoCachePath: $chocoCachePath" -ForegroundColor Yellow
+
+    if (-not (Test-Path -LiteralPath $chocoCachePath)) {
+        Write-Host "`tCreating chocoCachePath directory" -ForegroundColor Yellow
+        New-Item -Path $chocoCachePath -ItemType Directory -Force
+    }
     $VerbosePreference = 'SilentlyContinue'
-    Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host "`t`t[Executing Script]: " -NoNewline -ForegroundColor Yellow; Write-Host "$($helperUri)/$($script)`n" -ForegroundColor Cyan
-    Invoke-Expression ((New-Object net.webclient).DownloadString("$helperUri/$script"))
-}
-
-# Workaround Choco / boxstarter path too long error
-# https://github.com/chocolatey/boxstarter/issues/241
-$chocoCachePath = "$env:TEMP"
-
-if ([string]::IsNullOrEmpty($chocoCachePath)) {
-    $chocoCachePath = "$env:USERPROFILE\AppData\Local\Temp\chocolatey"
-}
-Write-Host "`n`nUsing chocoCachePath: $chocoCachePath" -ForegroundColor Yellow
-
-if (-not (Test-Path -LiteralPath $chocoCachePath)) {
-    Write-Host "`tCreating chocoCachePath directory" -ForegroundColor Yellow
-    New-Item -Path $chocoCachePath -ItemType Directory -Force
-}
-$VerbosePreference = 'SilentlyContinue'
-Write-Host "VerbosePreference = $VerbosePreference`n`n"
-Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host '[Starting]: ' -NoNewline -ForegroundColor Yellow; Write-Host "Bootstrap Script`n" -ForegroundColor Cyan
-
+    Write-Host "VerbosePreference = $VerbosePreference`n`n"
+    Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host '[Starting]: ' -NoNewline -ForegroundColor Yellow; Write-Host "Bootstrap Script`n" -ForegroundColor Cyan
+} catch {Write-Warning "Error chocoCachePath: Message:$($Error[0])"}
+#endregion choco cache path
 
 #--- Setting up Windows ---
+#region Windows Domain
 If (!(Get-CimInstance -Class Win32_ComputerSystem).PartOfDomain) {
     #$cred = Get-Credential -UserName lab\ps -Message 'To Autologon'
     #Install-BoxstarterPackage -Package "$($helperUri)/Add_To_Domain.ps1" -Credential $cred
@@ -57,13 +63,12 @@ If (!(Get-CimInstance -Class Win32_ComputerSystem).PartOfDomain) {
 } else {
     Write-Host 'Already part of a domain.' -ForegroundColor Green
 }
+#endregion Windows Domain
 
 
-#executeScript 'PStoolkit_Start.ps1'
 executeScript 'Execution_Policy.ps1';
 executeScript 'PSGallery.ps1';
 executeScript 'pstoolkit_install.ps1';
-#executeScript 'BaseApps.ps1';
 executeScript 'RemoveDefaultApps.ps1';
 executeScript 'FileExplorerSettings.ps1';
 
