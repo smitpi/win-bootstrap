7fe9c0336467eddaa82ca1e9fe302e030bb0b9c1
smitpi <pierre.smit@outlook.com>
2022-12-09 06:27:56 +0200

    Update pstoolkit_install.ps1

--- scripts/pstoolkit_install.ps1
+++ scripts/pstoolkit_install.ps1
@@ -16,26 +16,28 @@ Write-Host '-----------------------------------' -ForegroundColor DarkCyan; Writ
 
 #region PStoolkit
 Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host '[Installing]: ' -NoNewline -ForegroundColor Yellow; Write-Host "PSToolKit Module`n" -ForegroundColor Cyan
-$ModulePathCheck = 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit' 		
-$PSTemp = "$env:tmp\PSModules"
-if (Test-Path $PSTemp) {Remove-Item $PSTemp -Force -Recurse}
-if (Test-Path $ModulePathCheck) {Remove-Item $ModulePathCheck -Force -Recurse}
+try {
+	$ModulePathCheck = 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit' 		
+	$PSTemp = "$env:tmp\PSModules"
+	if (Test-Path $PSTemp) {Remove-Item $PSTemp -Force -Recurse}
+	if (Test-Path $ModulePathCheck) {Remove-Item $ModulePathCheck -Force -Recurse}
 			
-$PSDownload = New-Item $PSTemp -ItemType Directory -Force
-$ModulePath = New-Item $ModulePathCheck -ItemType Directory -Force
-$ModuleTMPDest = Join-Path $PSDownload.FullName -ChildPath 'PSToolKit.zip'
+	$PSDownload = New-Item $PSTemp -ItemType Directory -Force
+	$ModulePath = New-Item $ModulePathCheck -ItemType Directory -Force
+	$ModuleTMPDest = Join-Path $PSDownload.FullName -ChildPath 'PSToolKit.zip'
 	
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile('https://github.com/smitpi/PSToolKit/archive/refs/heads/master.zip', $ModuleTMPDest)
+	$wc = New-Object System.Net.WebClient
+	$wc.DownloadFile('https://github.com/smitpi/PSToolKit/archive/refs/heads/master.zip', $ModuleTMPDest)
 	
-Expand-Archive -Path $ModuleTMPDest -DestinationPath $PSDownload.FullName -Force
-$NewModule = Get-ChildItem -Directory "$($PSDownload.FullName)\PSToolKit-master\Output"
-Copy-Item -Path $NewModule.FullName -Destination $ModulePath.FullName -Recurse -Force
-Remove-Item $PSDownload -Force -Recurse
+	Microsoft.PowerShell.Archive\Expand-Archive -Path $ModuleTMPDest -DestinationPath $PSDownload.FullName -Force
+	$NewModule = Get-ChildItem -Directory "$($PSDownload.FullName)\PSToolKit-master\Output"
+	Copy-Item -Path $NewModule.FullName -Destination $ModulePath.FullName -Recurse -Force
+	Remove-Item $PSDownload -Force -Recurse
 
-Import-Module PSToolKit -Force
-Show-PSToolKit -ShowMetaData | Out-String
-Start-Sleep 3
+	Import-Module PSToolKit -Force
+	Show-PSToolKit -ShowMetaData | Out-String
+	Start-Sleep 3
+} catch {Write-Warning "Error installing PSToolKit: Message:$($Error[0])"}
 #endregion
 
 Write-Host '-----------------------------------' -ForegroundColor DarkCyan; Write-Host '[Creating]: ' -NoNewline -ForegroundColor Yellow; Write-Host "PowerShell Profile`n" -ForegroundColor Cyan
