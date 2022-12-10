852c42006488ba61c7467e9cabecab28a31c9a0a
Pierre Smit <pierre.smit@outlook.com>
2022-12-10 09:19:23 +0200

    Update dev_lab_setup.ps1

--- dev_lab_setup.ps1
+++ dev_lab_setup.ps1
@@ -10,8 +10,9 @@ $Boxstarter.AutoLogin = $true # Save my password securely and auto-login after a
 #. { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
 
 try {
+    Clear-Host
     Disable-UAC
-    $ConfirmPreference = 'None' #ensure installing PowerShell modules don't prompt on needed dependencies
+    Write-Host "Script to call: $($Boxstarter['ScriptToCall'])" -ForegroundColor Cyan
 } catch {Write-Warning "Error: Message:$($Error[0])"}
 
 # Get the base URI path from the ScriptToCall value
@@ -56,19 +57,13 @@ function executeScript {
 
 #region choco cache path
 try {
-    $chocoCachePath = "$env:TEMP"
+    $ChocoTemp = 'c:\temp\chocolatey'
+    if (-not(Test-Path $ChocoTemp)) {$chocoCachePath = New-Item -ItemType Directory -Path $ChocoTemp -Force}
+    else {$chocoCachePath = Get-Item $ChocoTemp}
 
-    if ([string]::IsNullOrEmpty($chocoCachePath)) {
-        $chocoCachePath = "$env:USERPROFILE\AppData\Local\Temp\chocolatey"
-    }
-    Write-Host "`n`nUsing chocoCachePath: $chocoCachePath" -ForegroundColor Yellow
+    Write-Host "`n`nUsing chocoCachePath: $($chocoCachePath.FullName)" -ForegroundColor Yellow
 
-    if (-not (Test-Path -LiteralPath $chocoCachePath)) {
-        Write-Host "`tCreating chocoCachePath directory" -ForegroundColor Yellow
-        New-Item -Path $chocoCachePath -ItemType Directory -Force
-    }
     $VerbosePreference = 'SilentlyContinue'
-    Write-Host "VerbosePreference = $VerbosePreference`n`n"
     Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host '[Starting]: ' -NoNewline -ForegroundColor Yellow; Write-Host "Bootstrap Script`n" -ForegroundColor Cyan
 } catch {Write-Warning "Error chocoCachePath: Message:$($Error[0])"}
 #endregion choco cache path
@@ -94,15 +89,15 @@ executeScript 'FileExplorerSettings.ps1';
 #region choco install
 
 # $VerbosePreference = 'SilentlyContinue'
-# $common = "--cache-location=$($chocoCachePath) --yes --limit-output --no-progress --source=chocolatey"
+$common = "--cache-location=$($chocoCachePath.FullName) --yes --limit-output --no-progress --source=chocolatey"
 
-# choco upgrade bandizip $common
-# choco upgrade cascadia-code-nerd-font $common
-# choco upgrade cascadiacodepl $common
-# choco upgrade GoogleChrome $common
-# choco upgrade microsoft-edge $common
-# choco upgrade microsoft-windows-terminal $common
-# choco upgrade pwsh $common
+choco upgrade bandizip $common
+choco upgrade cascadia-code-nerd-font $common
+choco upgrade cascadiacodepl $common
+choco upgrade GoogleChrome $common
+choco upgrade microsoft-edge $common
+choco upgrade microsoft-windows-terminal $common
+choco upgrade pwsh $common
 #endregion choco install
 
 #--- reenabling critical items ---
