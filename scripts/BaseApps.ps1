6d0e14c1708f99bf083215489154703b2b47b181
Pierre Smit <pierre.smit@outlook.com>
2022-12-10 17:56:10 +0200

    Update BaseApps.ps1

--- scripts/BaseApps.ps1
+++ scripts/BaseApps.ps1
@@ -1,14 +1,15 @@
 $AppsInstall = @('bandizip',
 	'cascadia-code-nerd-font',
 	'cascadiacodepl',
-	'GoogleChrome',
-	'microsoft-edge',
-	'microsoft-windows-terminal',
-	'pwsh')
+	'GoogleChrome'
+	#'microsoft-edge',
+	#'microsoft-windows-terminal',
+	#'pwsh'
+)
 
 foreach ($app in $AppsInstall) {
 	try {
-		Write-Host '[Installing]: ' -NoNewline -ForegroundColor Yellow; Write-Host "$($app)" -ForegroundColor Cyan -NoNewline
+		Write-Host '[Installing]: ' -NoNewline -ForegroundColor Yellow; Write-Host "$($app)" -ForegroundColor Cyan
 		choco upgrade $app --source chocolatey --accept-license --limit-output -y | Out-Null
 		#if ($LASTEXITCODE -ne 0) {Write-Host ' Failed' -ForegroundColor Red}
 		#if ($LASTEXITCODE -eq 0) {Write-Host ' Completed' -ForegroundColor Green}
