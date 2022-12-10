b0205743560e27ef0ff59a050c374e683f3e3f95
smitpi <pierre.smit@outlook.com>
2022-12-10 07:37:57 +0200

    Update dev_lab_setup.ps1

--- dev_lab_setup.ps1
+++ dev_lab_setup.ps1
@@ -74,16 +74,16 @@ executeScript 'FileExplorerSettings.ps1';
 
 #region choco install
 
-$VerbosePreference = 'SilentlyContinue'
-$common = "--cache-location=$($chocoCachePath) --yes --limit-output --no-progress --source=chocolatey"
+# $VerbosePreference = 'SilentlyContinue'
+# $common = "--cache-location=$($chocoCachePath) --yes --limit-output --no-progress --source=chocolatey"
 
-choco upgrade bandizip $common
-choco upgrade cascadia-code-nerd-font $common
-choco upgrade cascadiacodepl $common
-choco upgrade GoogleChrome $common
-choco upgrade microsoft-edge $common
-choco upgrade microsoft-windows-terminal $common
-choco upgrade pwsh $common
+# choco upgrade bandizip $common
+# choco upgrade cascadia-code-nerd-font $common
+# choco upgrade cascadiacodepl $common
+# choco upgrade GoogleChrome $common
+# choco upgrade microsoft-edge $common
+# choco upgrade microsoft-windows-terminal $common
+# choco upgrade pwsh $common
 #endregion choco install
 
 #--- reenabling critical items ---
