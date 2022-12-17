07f41270cec4915ebd8e75006ab0bf9977230ef2
smitpi <pierre.smit@outlook.com>
2022-12-18 01:08:37 +0200

    Update win_updates.ps1

--- scripts/win_updates.ps1
+++ scripts/win_updates.ps1
@@ -1,5 +1,8 @@
-#Enable-MicrosoftUpdate
-#Install-WindowsUpdate -getUpdatesFromMS -acceptEula -SuppressReboots
-Install-MSUpdates
+
+try {
+	Enable-MicrosoftUpdate
+	Install-WindowsUpdate -getUpdatesFromMS -acceptEula -SuppressReboots
+	Install-MSUpdates
+} catch {Write-Warning "Error: Message:$($Error[0])"}
 
 New-Item -Path $env:tmp\Bootstrap\win_updates.tmp -ItemType file -Force | Out-Null
