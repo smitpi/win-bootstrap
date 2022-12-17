fb9647be03c669ae11120edecf4f7329869cf3d7
smitpi <pierre.smit@outlook.com>
2022-12-18 01:01:53 +0200

    Update pstoolkit_install.ps1

--- scripts/pstoolkit_install.ps1
+++ scripts/pstoolkit_install.ps1
@@ -22,6 +22,35 @@ try {
 	else {$modulepath = Get-Item $modulepathcheck}
 	$ModChild = Get-ChildItem -Directory $ModulePath.FullName -ErrorAction SilentlyContinue
 
+	if (-not([string]::IsNullOrEmpty($ModChild))) {[version]$InstalledVer = ($ModChild | Sort-Object -Property Name -Descending)[0].Name}
+	[version]$OnlineVer = (Invoke-RestMethod 'https://raw.githubusercontent.com/smitpi/pstoolkit/master/Version.json').version
+	if (($InstalledVer -ne $OnlineVer) -or ([string]::IsNullOrEmpty($ModChild))) {
+		$PSTemp = "$env:tmp\PSModules"
+		if (Test-Path $PSTemp) {Remove-Item $PSTemp -Force -Recurse}
+		$PSDownload = New-Item $PSTemp -ItemType Directory -Force
+		$ModuleTMPDest = Join-Path $PSDownload.FullName -ChildPath 'PSToolKit.zip'
+	
+		$wc = New-Object System.Net.WebClient
+		$wc.DownloadFile('https://github.com/smitpi/PSToolKit/archive/refs/heads/master.zip', $ModuleTMPDest)
+	
+		Microsoft.PowerShell.Archive\Expand-Archive -Path $ModuleTMPDest -DestinationPath $PSDownload.FullName -Force
+		$NewModule = Get-ChildItem -Directory "$($PSDownload.FullName)\PSToolKit-master\Output"
+		Copy-Item -Path $NewModule.FullName -Destination $ModulePath.FullName -Recurse -Force
+		Remove-Item $PSDownload -Force -Recurse
+	} else {Write-Host 'Already installed' -ForegroundColor red}
+	Import-Module PSToolKit -Force
+	Show-PSToolKit -ShowMetaData | Out-String
+	Start-Sleep 3
+} catch {Write-Warning "Error installing PSToolKit: Message:$($Error[0])"}
+#endregion
+
+Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host '[Installing]: ' -NoNewline -ForegroundColor Yellow; Write-Host "PSToolKit Module`n" -ForegroundColor Cyan
+try {
+	$ModulePathCheck = 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit' 		
+	if (-not(Test-Path $ModulePathCheck)) {$ModulePath = New-Item $ModulePathCheck -ItemType Directory -Force}
+	else {$modulepath = Get-Item $modulepathcheck}
+	$ModChild = Get-ChildItem -Directory $ModulePath.FullName -ErrorAction SilentlyContinue
+
 	[version]$InstalledVer = ($ModChild | Sort-Object -Property Name -Descending)[0].Name
 	[version]$OnlineVer = (Invoke-RestMethod 'https://raw.githubusercontent.com/smitpi/pstoolkit/master/Version.json').version
 	if ($InstalledVer -ne $OnlineVer) {
