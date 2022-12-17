05b197cff60eebeb1d33d488835550cf77dcebdd
smitpi <pierre.smit@outlook.com>
2022-12-18 01:43:18 +0200

    Update win_updates.ps1

--- scripts/win_updates.ps1
+++ scripts/win_updates.ps1
@@ -1,8 +1,8 @@
 
 try {
-	Enable-MicrosoftUpdate
-	Install-WindowsUpdate
 	Install-MSUpdates
+	Install-WindowsUpdate -getUpdatesFromMS -acceptEula -SuppressReboots
+	Enable-MicrosoftUpdate
 } catch {Write-Warning "Error: Message:$($Error[0])"}
 
 New-Item -Path C:\Temp\Chocolatey\Bootstrap\win_updates.tmp -ItemType file -Force | Out-Null
