83a536c227ae34fcdb4ffd767fdda01a2abfa70d
smitpi <pierre.smit@outlook.com>
2022-12-18 01:45:06 +0200

    Update win_updates.ps1

--- scripts/win_updates.ps1
+++ scripts/win_updates.ps1
@@ -1,6 +1,10 @@
 
 try {
+	Import-Module PSToolKit -Force
+	Show-PSToolKit -ShowMetaData | Out-String
 	Install-MSUpdates
+} catch {Write-Warning "Error: Message:$($Error[0])"}
+try {
 	Install-WindowsUpdate -getUpdatesFromMS -acceptEula -SuppressReboots
 	Enable-MicrosoftUpdate
 } catch {Write-Warning "Error: Message:$($Error[0])"}
